54870b77ec7bb4d9447ae33b00e2c1ca30620dd2
kisumutalent <homiemusa@gmail.com>
2021-06-16 10:54:01 +0300

    Finish Redesign form

--- src/app/pagesApp/mulitforms/Business.js
+++ src/app/pagesApp/mulitforms/Business.js
@@ -1,41 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Business = ({formData, setForm, navigation}) =>{
-        const { legalBusinessName, businessPhone, websiteAddress } = formData
-    return(
-        <div className="pages">
+class Business extends Component{
+
+    continue = e =>{
+        e.preventDefault();
+        this.props.nextStep()
+    }
+    back = e =>{
+        e.preventDefault();
+        this.props.prevStep();
+    }
+
+    render(){
+
+        const { values, handleChange } = this.props;
+
+        return(
+            <div className="pages">
             <div className="row">
                 <div className="logery">
                     <div className="multform">
-                    <h3>Business Details</h3>
+                    <div className="row">
+                            <div className="col-md-9">
+                            <h3>Business Details</h3>
+                            </div>
+                            <div className="col-md-3">
+                            <button
+                                    type="submit" 
+                                    onClick={this.back}
+                                    class="btn btn-light btn-rounded">
+                                    <i class="fas fa-arrow-left"></i> &nbsp;Back
+                                
+                                </button>
+                            </div>
+                        </div>
+                  
+
                     </div>
                     <div className="card">
                         <div class="card-body">
             
                             <div class="form-outline">
                                 <label class="form-label" for="form1">Legal Business Number </label>
-                                <input type="text" name="legalBusinessName" value={legalBusinessName} onChange={setForm} id="form1" class="form-control" required/>
+                                <input type="text" name="legalBusinessName" 
+                                onChange={handleChange('legalBusinessName')}
+                                defaultValue={values.legalBusinessName} id="form1" class="form-control" required/>
                             </div>
                             <div class="form-outline">
                                     <label class="form-label" for="form1">Business Phone Number</label>
-                                    <input type="number" name="businessPhone" value={businessPhone} onChange={setForm} id="form1" class="form-control" required/>
+                                    <input type="number" name="businessPhone" 
+                                        onChange={handleChange('businessPhone')}
+                                        defaultValue={values.businessPhone} id="form1" class="form-control" required/>
                             </div>
                             <div class="form-outline">
                                     <label class="form-label" for="form1">Website URL</label>
-                                    <input type="text" name="websiteAddress" value={websiteAddress} onChange={setForm} id="form1" class="form-control" required/>
+                                    <input type="text" name="websiteAddress" 
+                                        onChange={handleChange('websiteAddress')}
+                                        defaultValue={values.websiteAddress} id="form1" class="form-control" required/>
                             </div><br/>
                             <button 
-                                    type="button" 
-                                    class="btn btn-primary"
-                                    onClick={() => navigation.next()}
-                                >
-                                    Next
+                                    onClick={this.continue}
+                                    class="btn btn-primary btn-block mb-4">
+                                    Continue &nbsp; <i class="fas fa-arrow-right"></i>
                                 </button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+ 
+        )
+    }
 }
 export default Business;
--- src/app/pagesApp/mulitforms/Representative.js
+++ src/app/pagesApp/mulitforms/Representative.js
@@ -1,52 +1,83 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Representative = ({formData, setForm, navigation}) =>{
- 
-        const { ownerName, ownerEmail, ownerTitle, ownerMobilePhoneteCd, ownerDob } = formData
-    return(
-        <div className="pages">
-                <div className="row">
-                <div className="logery">
-                <div className="multform">
-                <h3>Business Owner Informations</h3>
-                </div>
-                <div className="card">
-                    <div class="card-body">
-                        <div class="form-outline">
-                        <label class="form-label" for="form1">Owner Church Name</label>
-                        <input type="text" name="ownerName" value={ownerName} onChange={setForm} id="form1" class="form-control" required/>
-                        </div>
-                        <div class="form-outline">
-                                <label class="form-label" for="form1">Owner Email</label>
-                                <input type="email" name="ownerEmail" value={ownerEmail} onChange={setForm} id="form1" class="form-control"  required/>
-                        </div>
-                        <div class="form-outline">
-                                <label class="form-label" for="form1">Owner Title</label>
-                                <input type="text" name="ownerTitle" value={ownerTitle} onChange={setForm} onChange={setForm} id="form1" class="form-control" required />
-                        </div>
-                        <div class="form-outline">
-                                <label class="form-label" for="form1">Owner Mobile Phone</label>
-                                <input type="number" name="ownerMobilePhoneteCd" value={ownerMobilePhoneteCd} onChange={setForm} id="form1" class="form-control" required/>
-                        </div>
-                        <div class="form-outline">
-                                <label class="form-label" for="form1">Owner Date of Birth</label>
-                                <input type="date" name="ownerDob" value={ownerDob} onChange={setForm} id="form1" class="form-control" required/>
-                        </div>
+class Representative extends Component{
 
-                        <br/>
-                        <button 
-                                type="button" 
-                                class="btn btn-primary"
-                                onClick={() => navigation.next()}
-                                >
-                                Next
-                        </button>
+        continue = e =>{
+                e.preventDefault();
+                this.props.nextStep()
+            }
+            back = e =>{
+                e.preventDefault();
+                this.props.prevStep();
+            }
+        render(){
+                const { values, handleChange } = this.props
+                return(
+                        <div className="pages">
+                                <div className="row">
+                                <div className="logery">
+                                <div className="multform">
+                                <div className="row">
+                            <div className="col-md-9">
+                            <h3>Business Owner Informations</h3>
+                            </div>
+                            <div className="col-md-3">
+                            <button
+                                    type="submit" 
+                                    onClick={this.back}
+                                    class="btn btn-light btn-rounded">
+                                    <i class="fas fa-arrow-left"></i>&nbsp; Back
+                                </button>
+                            </div>
                         </div>
-                </div>
-        </div>
-        </div>
-        </div>
-    )
+                        </div>
+                         <div className="card">
+                                    <div class="card-body">
+                                        <div class="form-outline">
+                                        <label class="form-label" for="form1">Owner Church Name</label>
+                                        <input type="text" name="ownerName" 
+                                        onChange={handleChange('ownerName')}
+                                        defaultValue={values.ownerName} id="form1" class="form-control" required/>
+                                        </div>
+                                        <div class="form-outline">
+                                                <label class="form-label" for="form1">Owner Email</label>
+                                                <input type="email" name="ownerEmail" 
+                                                onChange={handleChange('ownerEmail')}
+                                                defaultValue={values.ownerEmail} id="form1" class="form-control"  required/>
+                                        </div>
+                                        <div class="form-outline">
+                                                <label class="form-label" for="form1">Owner Title</label>
+                                                <input type="text" name="ownerTitle" 
+                                                onChange={handleChange('ownerTitle')}
+                                                defaultValue={values.ownerTitle} id="form1" class="form-control" required />
+                                        </div>
+                                        <div class="form-outline">
+                                                <label class="form-label" for="form1">Owner Mobile Phone</label>
+                                                <input type="number" name="ownerMobilePhoneteCd" 
+                                                onChange={handleChange('ownerMobilePhoneteCd')}
+                                                defaultValue={values.ownerMobilePhoneteCd} id="form1" class="form-control" required/>
+                                        </div>
+                                        <div class="form-outline">
+                                                <label class="form-label" for="form1">Owner Date of Birth</label>
+                                                <input type="date" name="ownerDob" 
+                                               onChange={handleChange('ownerDob')}
+                                               defaultValue={values.ownerDob} id="form1" class="form-control" required/>
+                                        </div>
+                
+                                        <br/>
+                                        <button 
+                                                onClick={this.continue}
+                                                class="btn btn-primary btn-block mb-4">
+                                                Continue &nbsp; <i class="fas fa-arrow-right"></i>
+                                        </button>
+                                        </div>
+                                </div>
+                        </div>
+                        </div>
+                        </div>
+                    )
+
+        }
+
 }
-
 export default Representative;
